c3de6dce18580262e0912f230bf51f95c9222803
Jamin Amos Yeates <jamin.amos.yeates@mni.thm.de>
2023-10-29 13:00:03 +0100

    update: adding our colors to tailwind config

--- components/ProtocolContainer.tsx
+++ components/ProtocolContainer.tsx
@@ -13,14 +13,14 @@ const ProtocolContainer = ({
 }: ProtocolContainerProps & HTMLAttributes<HTMLDivElement>) => {
   return (
     <div className={className}>
-      <div className="grid grid-rows-2 items-center rounded-xl border-2 border-slate-300 justify-center p-2 shadow hover:shadow-lg hover:cursor-pointer">
+      <div className="grid grid-rows-2 items-center rounded-xl border-2 border-outline justify-center p-2 shadow hover:shadow-lg hover:cursor-pointer">
         <div className="row-span-1 grid grid-flow-col items-center justify-start gap-3">
-          <div className="col-span-1 rounded-full bg-teal-900 w-10 h-10 flex items-center justify-center text-white">
+          <div className="col-span-1 rounded-full bg-mni w-10 h-10 flex items-center justify-center text-white">
             FS
           </div>
           <div className="col-span-1">
-            <h2>{protocol.type}</h2>
-            <h4 className="text-slate-600">09.08.2023 - 14:04 bis 15:55</h4>
+            <h2 className="text-primary">{protocol.type}</h2>
+            <h4 className="text-secondary">09.08.2023 - 14:04 bis 15:55</h4>
           </div>
         </div>
         <div className="row-span-1 flex">
@@ -28,7 +28,7 @@ const ProtocolContainer = ({
             return (
               <div
                 key={topic}
-                className="rounded-full bg-slate-600 text-white flex items-center justify-center px-5 py-1 m-1"
+                className="rounded-full bg-secondary text-white font-bold flex items-center justify-center px-5 py-1 m-1"
               >
                 {topic}
               </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,14 +9,16 @@ export default function Home() {
     end: Date.now(),
     topics: ["Exkursion", "Aufgaben", "..."],
   };
-  
+
   return (
-    <div className="container">
-      <h1>Protokolle</h1>
+    <div className="items-center justify-center m-5">
+      <h1 className="text-primary">Protokolle</h1>
       <hr></hr>
-      <div className="flex flex-wrap m-2">
-        {[...Array(10)].map((x) => {
-          return <ProtocolContainer key={x} protocol={protocol} className="m-2" />;
+      <div className="flex flex-wrap m-2 justify-center">
+        {[...Array(20)].map((x) => {
+          return (
+            <ProtocolContainer key={x} protocol={protocol} className="m-2" />
+          );
         })}
       </div>
     </div>
