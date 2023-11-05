36ce7eebfbc07e9fe6f1d483f56305c0e18bd759
Jamin Amos Yeates <jamin.amos.yeates@mni.thm.de>
2023-11-05 18:00:23 +0100

    Landing page

--- components/ProtocolContainer.tsx
+++ components/ProtocolContainer.tsx
@@ -7,20 +7,42 @@ export interface ProtocolContainerProps {
   protocol: Protocol;
 }
 
+export function formatProtocolDate(protocol: Protocol): String {
+  let start = new Date(protocol.start_timestamp * 1000);
+  let end = new Date(protocol.end_timestamp * 1000);
+  const optionsDate: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+  };
+  const optionsTime: Intl.DateTimeFormatOptions = {
+    hour: "numeric",
+    minute: "numeric",
+  };
+
+  return (
+    start.toLocaleDateString("de-DE", optionsDate) +
+    " - " +
+    start.toLocaleTimeString("de-DE", optionsTime) + 
+    " bis " +
+    end.toLocaleTimeString("de-DE", optionsTime)
+  );
+}
+
 const ProtocolContainer = ({
   className,
   protocol,
 }: ProtocolContainerProps & HTMLAttributes<HTMLDivElement>) => {
   return (
     <div className={className}>
-      <div className="grid grid-rows-2 items-center rounded-xl border-2 border-slate-300 justify-center p-2 shadow hover:shadow-lg hover:cursor-pointer">
+      <div className="grid grid-rows-2 items-center rounded-xl border-2 border-outline justify-center p-2 shadow hover:shadow-lg hover:cursor-pointer">
         <div className="row-span-1 grid grid-flow-col items-center justify-start gap-3">
-          <div className="col-span-1 rounded-full bg-teal-900 w-10 h-10 flex items-center justify-center text-white">
+          <div className="col-span-1 rounded-full bg-mni w-10 h-10 flex items-center justify-center text-white">
             FS
           </div>
           <div className="col-span-1">
-            <h2>{protocol.type}</h2>
-            <h4 className="text-slate-600">09.08.2023 - 14:04 bis 15:55</h4>
+            <h2 className="text-primary">{protocol.protocol_type}</h2>
+            <h4 className="text-secondary">{formatProtocolDate(protocol)}</h4>
           </div>
         </div>
         <div className="row-span-1 flex">
@@ -28,7 +50,7 @@ const ProtocolContainer = ({
             return (
               <div
                 key={topic}
-                className="rounded-full bg-slate-600 text-white flex items-center justify-center px-5 py-1 m-1"
+                className="rounded-full bg-secondary text-white font-bold flex items-center justify-center px-5 py-1 m-1"
               >
                 {topic}
               </div>
