12825fde60e38a65c4f5ea757065038d1680b39d
Jamin Amos Yeates <jamin.amos.yeates@mni.thm.de>
2023-11-05 18:58:56 +0100

    update: added markdown renderer

--- pages/protocol/index.tsx
+++ pages/protocol/index.tsx
@@ -1,7 +1,23 @@
-const Protocol = () => {
-    return (<div>
-        Protocol view page
-    </div>)
+import Protocol from "@/components/Protocol";
+import { useEffect, useState } from "react";
+import Markdown from "react-markdown";
+
+const ProtocolView = () => {
+  let [markdown, setMarkdown] = useState<string>("");
+
+  useEffect(() => {
+    //TODO: daten fetchen
+    loadProtocols();
+  }, []);
+
+  async function loadProtocols() {
+    let response = await fetch("http://localhost:3000/protocol/0.json");
+    let json = await response.json();
+    let protocol = json as Protocol;
+    setMarkdown(protocol.content);
+  }
+
+  return <Markdown>{markdown}</Markdown>;
 };
 
-export default Protocol;
+export default ProtocolView;
