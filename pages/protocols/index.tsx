a9426ad6391ff18878525a7e46ba498c7db0a7db
Merta <Mertali27062001@hotmail.com>
2023-11-05 23:44:17 +0100

    minor changes

--- pages/protocols/index.tsx
+++ pages/protocols/index.tsx
@@ -1,21 +1,27 @@
 import Protocol from "@/components/Protocol";
 import { useEffect, useState } from "react";
 import Markdown from "react-markdown";
+import { formatProtocolDate } from "@/components/ProtocolContainer"
 
 const ProtocolView = () => {
   let [markdown, setMarkdown] = useState<string>("");
 
+
   useEffect(() => {
+    let protocolId = 0;
     //TODO: daten fetchen
-    loadProtocols();
+    loadProtocols(protocolId);
   }, []);
 
-  async function loadProtocols() {
-    let response = await fetch("http://localhost:3000/protocol/0.json");
+
+  async function loadProtocols(id: number) {
+    let response = await fetch(`http://localhost:3000/protocols/${id}.json`);
     let json = await response.json();
     let protocol = json as Protocol;
     setMarkdown(protocol.content);
   }
+
+
 
   return <Markdown>{markdown}</Markdown>;
 };
