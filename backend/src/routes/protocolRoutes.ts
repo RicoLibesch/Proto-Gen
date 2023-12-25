ca39e669c47ddb8353afa39784268680dda480e3
Jonas Nickel <Jonas Nickel>
2023-12-25 20:20:53 +0100

    add export endpoint to download all protocols

--- backend/src/routes/protocolRoutes.ts
+++ backend/src/routes/protocolRoutes.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 export const router = express.Router();
-import { getProtocols, createProtocol, getProtocol } from '../controllers/protocolController';
+import { getProtocols, createProtocol, getProtocol, getAllProtocols } from '../controllers/protocolController';
 import { verifyAccessToken, isRecorder } from '../middleware/authMiddleware';
 
 router.route("/").get(getProtocols);
 router.route("/").post(verifyAccessToken, isRecorder, createProtocol);
+router.route("/export").get(getAllProtocols);
 router.route("/:id").get(getProtocol);
