538b1b7b3c302681700a73aab0c036da94767b56
Jonas Nickel <Jonas Nickel>
2023-12-25 20:25:55 +0100

    export protocols endpoints requires admin rights

--- backend/src/routes/protocolRoutes.ts
+++ backend/src/routes/protocolRoutes.ts
@@ -1,9 +1,9 @@
 import express from 'express';
 export const router = express.Router();
 import { getProtocols, createProtocol, getProtocol, getAllProtocols } from '../controllers/protocolController';
-import { verifyAccessToken, isRecorder } from '../middleware/authMiddleware';
+import { verifyAccessToken, isRecorder, isAdmin } from '../middleware/authMiddleware';
 
 router.route("/").get(getProtocols);
 router.route("/").post(verifyAccessToken, isRecorder, createProtocol);
-router.route("/export").get(getAllProtocols);
+router.route("/export").get(verifyAccessToken, isAdmin, getAllProtocols);
 router.route("/:id").get(getProtocol);
