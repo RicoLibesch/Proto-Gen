7c615fb74a6d5c51fa1a6b1b7d5a369bdc500b82
Jonas Nickel <jonas.nickel@mni.thm.de>
2024-01-14 18:38:41 +0100

    Fix: wrong validations

--- backend/src/routes/protocolRoutes.ts
+++ backend/src/routes/protocolRoutes.ts
@@ -1,9 +1,9 @@
 import express from 'express';
 export const router = express.Router();
 import { getProtocols, createProtocol, getProtocol, getAllProtocols } from '../controllers/protocolController';
-import { verifyAccessToken, isRecorder, isAdmin } from '../middleware/authMiddleware';
+import { verifyAccessToken, isAdmin, isAdminOrRecorder } from '../middleware/authMiddleware';
 
 router.route("/").get(getProtocols);
-router.route("/").post(verifyAccessToken, isRecorder, createProtocol);
+router.route("/").post(verifyAccessToken, isAdminOrRecorder, createProtocol);
 router.route("/export").get(verifyAccessToken, isAdmin, getAllProtocols);
 router.route("/:id").get(getProtocol);
